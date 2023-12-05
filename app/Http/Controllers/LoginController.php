4e6a22e35a65d77e8d0bd3bc2e6620ddde320185
eduardostc <eduardostc@hotmail.com>
2023-12-05 15:59:43 -0300

    "Cadastrar usuário na página login Concluido"

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
-use Illuminate\Auth\Events\Authenticated;
-use Illuminate\Http\Request;
+use App\Http\Requests\LoginUserRequest;
+use Exception;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use App\Models\User;
 
 class LoginController extends Controller
 {
@@ -34,6 +37,48 @@ class LoginController extends Controller
             //Se as credencias estão corretas, redirecionar o usuário
             return redirect()->route('dashboard.index');
 
+    }
+
+    public function create(){
+        //carregar a view
+        return view('login.create');
+    }
+
+    public function store(LoginUserRequest $request){
+        //validar o formulário
+        $request->validated();
+
+        //marca o ponto inicial de uma transação
+        DB::beginTransaction();
+        
+        try{
+            //Cadastrar no bd na tabela usuários
+           $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password,
+            ]);
+
+            // Salvar log
+            Log::info('Usuário cadastrado.', ['id' => $user->id ]);
+
+            //operação é concluída com êxito
+            DB::commit();
+
+            //Redirecionar o usuário, enviar a msg de sucesso
+            return redirect()->route('login.index')->with('success', 'Usuário cadastrado com sucesso!');
+
+        } catch (Exception $e) {
+            //throw $th;
+             // Salvar log
+             Log::info('Usuário não cadastrado.', ['error' => $e->getMessage()]);
+
+             // Operação não é concluída com êxito
+             DB::rollBack();
+ 
+             // Redirecionar o usuário, enviar a mensagem de erro
+             return back()->withInput()->with('error', 'Usuário não cadastrado!');
+        }
     }
     //Deslogar o usuário
     public function destroy(){
--- resources/views/login/create.blade.php
+++ resources/views/login/create.blade.php
@@ -7,33 +7,44 @@
                 <main>
                     <div class="container">
                         <div class="row justify-content-center">
-                            <div class="col-lg-5">
+                            <div class="col-lg-7">
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
                                     <div class="card-header"><h3 class="text-center font-weight-light my-4">Área Restrita</h3></div>
                                     <div class="card-body">
 
                                         <x-alert/>
 
-                                        <form action="{{ route('login.process') }}" method="POST">
+                                        <form action="{{ route('login.store-user') }}" method="POST">
                                         @csrf
+
+                                            <div class="form-floating mb-3">
+                                                <input type="text" name="name" class="form-control" id="name"  placeholder="Nome completo" value="{{ old('name') }}"/>
+                                                <label for="name">Nome</label>
+                                            </div>
 
                                             <div class="form-floating mb-3">
                                                 <input type="email" name="email" class="form-control" id="email"  placeholder="E-mail de usuário" value="{{ old('email') }}"/>
                                                 <label for="email">E-mail </label>
                                             </div>
+
                                             <div class="form-floating mb-3">
                                                 <input type="password" name="password" class="form-control" id="password"  placeholder="Senha" value="{{ old('password') }} />
                                                 <label for="password">Senha</label>
                                             </div>
-                                            
-                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                                                <a class="small text-decoration-none" href="#">Esqueceu a senha?</a>
-                                                <button type="submit" class="btn btn-danger">Acessar</button>
+
+                                            <div class="mt-4 mb-0">
+                                                <div class="d-grid">
+                                                    <button type="submit" class="btn btn-danger btn-block" >
+                                                    Cadastrar</button>
+                                                </div>
                                             </div>
+                                            
                                         </form>
                                     </div>
-                                    <div class="card-footer text-center py-3">
-                                        <div class="small">Precisa de uma conta? <a href="{{ route('login.create-user') }}" class="text-decoration-none">Inscrever-se!</a></div>
+                                      <div class="card-footer text-center py-3">
+                                        <div class="small">
+                                        <a class="text-decoration-none" 
+                                        href="{{ route('login.index') }}">Clique aqui</a> para acessar</div>
                                     </div>
                                 </div>
                             </div>
